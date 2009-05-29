7038960f8061d9d4fc09acfaa1881064a637294b
Dominic Sayers <dominic_sayers@hotmail.com>
2009-05-29 14:54:50 +0100

    Version 0.15 - Now released under the BSD license

---
+++ examples/example-both.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<title>ezUser registration page example - PHP/Object</title>
+</head>
+
+<body>
+<?php
+	require_once '../ezUser.php';
+	ezUserUI::getContainer('controlpanel');
+	echo '<br style="clear:left;" /><hr />';
+	ezUserUI::getContainer('account=new');
+?>
+</body>
+
+</html>
